a7896d23af335783cdf202e1478a9a089148f8fa
ZEDT-FOX <zfirht@hotmail.com>
2021-12-14 14:31:25 +0700

    Shuffle card on deck

--- Assets/Script/Card/CardHandler.cs
+++ Assets/Script/Card/CardHandler.cs
@@ -16,29 +16,40 @@ public class CardHandler : MonoBehaviour
     }
 
     void Update() {
-        if((deck.slot.Count != 0) && (cardList.Count < deck.slot.Count)) {
-            SpawnCards();
-            SetCardsValue();
+        SetDeck();
+    }
+
+    private void SetDeck() {    
+        void SpawnCards() {
+            for(int i = 0; i < deck.slot.Count; i++) {
+                GameObject cardInstance = Instantiate(cardPrefab, deck.slot[i].position, Quaternion.identity, parent);
+                cardList.Add(cardInstance);
+            }
         }
-    }
 
-    private void SpawnCards() {
-        for(int i = 0; i < deck.slot.Count; i++) {
-            GameObject cardInstance = Instantiate(cardPrefab, deck.slot[i].position, Quaternion.identity, parent);
-            cardList.Add(cardInstance);
+        void SetCardsValue() {
+            for(int i = 0; i < cardList.Count; i++) {
+                Card cardGenerator = cardList[i].GetComponent<Card>();
+                cardGenerator.value = gameHandler.operands[i];
+            }
         }
-    }
 
-    private void SetCardsValue() {
-        for(int i = 0; i < cardList.Count; i++) {
-            Card cardGenerator = cardList[i].GetComponent<Card>();
-            cardGenerator.value = gameHandler.operands[i];
+        void Shuffle() {
+            List<Vector3> slotPosition = new List<Vector3>();
+
+            foreach(Transform slot in deck.slot) slotPosition.Add(slot.position);
+            for(int i = 0; i < cardList.Count + 1; i++) {
+                int index = Random.Range(0, slotPosition.Count);
+                cardList[i].transform.position = slotPosition[index];
+
+                slotPosition.RemoveAt(index);
+            }
         }
-    }
 
-    /* TODO: Randomize card position on deck
-    private void ShuffleDeck() {
+        if((deck.slot.Count == 0) || (cardList.Count >= deck.slot.Count)) return;
 
+        SpawnCards();
+        SetCardsValue();
+        Shuffle();
     }
-    */
 }
