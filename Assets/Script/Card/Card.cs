257979fb21560d4322272f637c149ab301ab3348
ZEDT-FOX <zfirht@hotmail.com>
2021-12-15 11:22:48 +0700

    Draggable card on deck slot

--- Assets/Script/Card/Card.cs
+++ Assets/Script/Card/Card.cs
@@ -1,12 +1,46 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using TMPro;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public TextMeshProUGUI valueText;
     public int value;
+    // TODO: set droppedOnBoard to true when card dragged onto board slot
+    [HideInInspector] public bool droppedOnBoard, onEndDrag;
+
+    private RectTransform rectTransform;
+    private Canvas cardCanvas;
+    private CanvasGroup cardCanvasGroup;
+    private Vector3 defaultPosition;
+
+    void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+        cardCanvas = gameObject.transform.root.GetComponent<Canvas>();
+        cardCanvasGroup = GetComponent<CanvasGroup>();
+    }
 
     void Start() {
         valueText.text = value.ToString();
+        defaultPosition = rectTransform.localPosition;
+    }
+
+    void LateUpdate() {
+        if(!droppedOnBoard && onEndDrag) rectTransform.localPosition = defaultPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        onEndDrag = false;
+        droppedOnBoard = false;
+        cardCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        rectTransform.anchoredPosition += eventData.delta / cardCanvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        onEndDrag = true;
+        cardCanvasGroup.blocksRaycasts = true;
     }
 }
