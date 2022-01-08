67e5ce5432eed37b16db1693d734705a4e624b57
ZEDT-FOX <zfirht@hotmail.com>
2022-01-08 12:19:15 +0700

    Fixed editor bug caused by if all operator disabled

--- Assets/Editor/FormulaEditor.cs
+++ Assets/Editor/FormulaEditor.cs
@@ -36,12 +36,31 @@ public class FormulaEditor : Editor
         }
 
         void SetOperatorsToggle() {
+            bool LastOneToggled() {
+                int activeOperator = 0;
+
+                for(int i = 0; i < gameHandler.OperatorsToggle.Length; i++) {
+                    if(gameHandler.OperatorsToggle[i] == true) activeOperator++;
+                }
+
+                return activeOperator == 1;
+            }
+
+            void Toggle(int index, string label) {
+                bool toogleable = true;
+
+                if(LastOneToggled() && gameHandler.OperatorsToggle[index]) toogleable = false;
+
+                GUI.enabled = toogleable;
+                gameHandler.OperatorsToggle[index] = EditorGUILayout.Toggle(label, gameHandler.OperatorsToggle[index]);
+            }
+
             EditorGUILayout.LabelField("Operator Toggle");
             EditorGUI.indentLevel++;
-                gameHandler.OperatorsToggle[0] = EditorGUILayout.Toggle("Adder", gameHandler.OperatorsToggle[0]);
-                gameHandler.OperatorsToggle[1] = EditorGUILayout.Toggle("Substractor", gameHandler.OperatorsToggle[1]);
-                gameHandler.OperatorsToggle[2] = EditorGUILayout.Toggle("Multiplicator", gameHandler.OperatorsToggle[2]);
-                gameHandler.OperatorsToggle[3] = EditorGUILayout.Toggle("Devider", gameHandler.OperatorsToggle[3]);
+                Toggle(0, "Adder");
+                Toggle(1, "Substractor");
+                Toggle(2, "Multiplicator");
+                Toggle(3, "Divider");
             EditorGUI.indentLevel--;
         }
 
